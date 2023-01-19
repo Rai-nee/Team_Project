8b9c4ecf96994627aa22bc4f241f0d5873c7766b
mikky-j <moregunwa@gmail.com>
2023-01-19 21:23:48 +0100

    Finalized Calculator

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -12,48 +12,33 @@ let calculator = document.getElementById("calculator");
 // This is used to know if we should clear after we press a button
 // This is set to `true` when the `=` button is pressed
 let shouldClear = false;
-// Set's the class of the calculator to `dark`
-dark_button.addEventListener("click", () => {
-  calculator.className = "dark phone";
-});
-// Set's the class of the calculator to `dark`
-light_button.addEventListener("click", () => {
-  calculator.className = "light phone";
-});
-
 // This just removes the last character of the `total`'s value
-backspace_button.addEventListener("click", () => {
+const handleBackspace = () => {
   total.innerHTML = total.innerHTML.substring(0, total.innerHTML.length - 1);
-});
-// This just clears the preview and the total
-clear_button.addEventListener("click", () => {
-  total.innerHTML = "";
-  preview.innerHTML = "";
-});
-
-// This handles operators when they are pressed in the calculator
-const handleOperators = (event) => {
-  let element = event.target;
-  if (total.innerHTML.length != 0) {
-    preview.innerHTML += total.innerHTML + ` ${element.innerHTML} `;
-    total.innerHTML = "";
-  } else if (element.innerHTML == "-") {
-    total.innerHTML += element.innerHTML;
-  }
-};
-
-// This handles button when they are pressed in the calculator
-const handleButtons = (event) => {
-  let element = event.target;
-  if (preview.innerHTML.length == 0 && shouldClear) {
-    total.innerHTML = element.innerHTML;
-  } else {
-    total.innerHTML += element.innerHTML;
-  }
   shouldClear = false;
 };
+// This handls clearing of the `total` and the `preview` boxes
+const handleClear = () => {
+  total.innerHTML = "";
+  preview.innerHTML = "";
+};
+// This handles the adding of text to the preview and total boxes
+const handleInput = (key) => {
+  if (preview.innerHTML.length == 0 && shouldClear && /[0-9\-]+/.test(key)) {
+    total.innerHTML = key;
+    shouldClear = false;
+  } else if (
+    /[\-+/x*%]/.test(key) &&
+    (/\d$/.test(preview.innerHTML) || preview.innerHTML.length == 0)
+  ) {
+    preview.innerHTML += total.innerHTML + ` ${key} `;
+    total.innerHTML = "";
+  } else if (/\d+/.test(key)) {
+    total.innerHTML += key;
+  }
+};
 // This is what calculates all the inputs
-equal.addEventListener("click", () => {
+const getResult = () => {
   let acc = "";
   for (let i = 0; i < preview.innerHTML.length; i++) {
     if (preview.innerHTML[i] === "x") {
@@ -62,9 +47,43 @@ equal.addEventListener("click", () => {
       acc += preview.innerHTML[i];
     }
   }
-  total.innerHTML = eval(acc + total.innerHTML);
+  let result = eval(acc + total.innerHTML);
+  if (result == parseInt(result)) {
+    total.innerHTML = result;
+  } else {
+    total.innerHTML = parseFloat(result).toPrecision(5);
+  }
   preview.innerHTML = "";
   shouldClear = true;
+};
+// This handles operators when they are pressed in the calculator
+const handleOperators = (event) => {
+  let element = event.target;
+  handleInput(element.innerHTML);
+};
+// This handles button when they are pressed in the calculator
+const handleButtons = (event) => {
+  let element = event.target;
+  handleInput(element.innerHTML);
+};
+// Event listeners
+// Set's the class of the calculator to `dark`
+dark_button.addEventListener("click", () => {
+  calculator.className = "dark phone";
+});
+// Set's the class of the calculator to `dark`
+light_button.addEventListener("click", () => {
+  calculator.className = "light phone";
+});
+equal.addEventListener("click", () => {
+  getResult();
+});
+// This just clears the preview and the total
+clear_button.addEventListener("click", () => {
+  handleClear();
+});
+backspace_button.addEventListener("click", () => {
+  handleBackspace();
 });
 // This loops over the buttons and adds the `handleButtons` function to all of them
 for (let index = 0; index < buttons.length; index++) {
@@ -76,3 +95,15 @@ for (let index = 0; index < operators.length; index++) {
   const element = operators[index];
   element.addEventListener("click", handleOperators);
 }
+window.addEventListener("keydown", (ev) => {
+  ev.preventDefault();
+  if (/[0-9+\-*%./]+/.test(ev.key)) {
+    handleInput(ev.key);
+  } else if (ev.keyCode == 13) {
+    getResult();
+  } else if (ev.keyCode == 8) {
+    handleBackspace();
+  } else if (ev.keyCode == 27) {
+    handleClear();
+  }
+});
